bff6abf036f7355d350c731300395a91e7431e1f
Bartłomiej Kuras <hash.deb@gmail.com>
2020-12-02 21:40:35 +0100

    Day 3 boilerplate

---
+++ day3/src/main.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+
+fn part1(input: &[u32]) -> Result<u32> {
+    todo!()
+}
+
+fn part2(input: &[u32]) -> Result<u32> {
+    todo!()
+}
+
+fn main() -> Result<()> {
+    let input: Vec<u32> = common::std_input_vec()?;
+
+    println!("Part1: {}", part1(&input)?);
+    println!("Part2: {}", part2(&input)?);
+
+    Ok(())
+}
